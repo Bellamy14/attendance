997f330822f825311afd0ce57d8062b8821240d8
Bellamy14 <46901791+Bellamy14@users.noreply.github.com>
2022-11-20 17:32:15 -0500

    Initial commit

---
+++ includes/footer.php
@@ -0,0 +1,31 @@
+
+<div id = "footer" class = "p-3 mb-3 bg-primary text-white fixed-bottom" >
+    
+     <p class="text-center">Copyright &copy; - IT Conference Attendance System <?php echo date('y'); ?> </p>
+
+</div>
+
+
+
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+
+<script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+
+<script>
+
+  $( function() {
+
+    $( "#DateofBirth" ).datepicker({
+      changeMonth: true,
+      changeYear: true,
+      yearRange: "-100:+0",
+      dateFormat: "yy-mm-dd"
+    });
+  } );
+  </script>
+
+</body>
+
+</html>
